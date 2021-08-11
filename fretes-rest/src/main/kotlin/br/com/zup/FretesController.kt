41ec64825098d9434879aa3327dbae765154a0c9
FelipeGadelha <felipegadelha90@gmail.com>
2021-08-10 22:38:01 -0300

    created convertion in error grpc for error rest

--- fretes-rest/src/main/kotlin/br/com/zup/FretesController.kt
+++ fretes-rest/src/main/kotlin/br/com/zup/FretesController.kt
@@ -1,8 +1,14 @@
 package br.com.zup
 
+import com.google.protobuf.Any
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import io.grpc.protobuf.StatusProto
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.QueryValue
+import io.micronaut.http.exceptions.HttpStatusException
 import javax.inject.Inject
 
 @Controller
@@ -14,8 +20,24 @@ class FretesController(@Inject val gRpcClient: FretesServiceGrpc.FretesServiceBl
             .newBuilder()
             .setCep(cep)
             .build()
+        try {
         val response = gRpcClient.calculaFrete(request)
         return FreteRs(cep = response.cep, valor = response.valor)
+        } catch (ex: StatusRuntimeException){
+            val description = ex.status.description
+            val statusCode = ex.status.code
+            if (statusCode == Status.Code.INVALID_ARGUMENT){
+                throw HttpStatusException(HttpStatus.BAD_REQUEST, description)
+            }
+            if (statusCode == Status.Code.PERMISSION_DENIED){
+                val statusProto = StatusProto
+                    .fromThrowable(ex) ?: throw HttpStatusException(HttpStatus.FORBIDDEN, description)
+                val anyDetails: Any = statusProto.detailsList[0]
+                val errorDetails = anyDetails.unpack(ErrorDetails::class.java)
+                throw HttpStatusException(HttpStatus.FORBIDDEN, "${errorDetails.code}: ${errorDetails.message}")
+            }
+            throw HttpStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.message)
+        }
     }
 }
 
